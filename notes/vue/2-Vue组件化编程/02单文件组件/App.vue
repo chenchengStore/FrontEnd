98bab8a868a13a1241d8d0e324805ac6d0e30627
chenchengStore <139768726+chenchengStore@users.noreply.github.com>
2023-07-24 19:15:26 +0800

    Add files via upload

---
+++ notes/vue/2-Vue组件化编程/02单文件组件/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="demo">
+    <School/>
+    <Student/>
+  </div>
+</template>
+
+<script>
+import School from './School.vue'
+import Student from './Student.vue'
+export default {
+    // '<v'可以快捷输入模板内容
+    name:'App',
+    components:{
+        School,
+        Student
+    }
+}
+</script>
+
+<style>
+    .demo{
+        background-color: red;
+    }
+</style>
