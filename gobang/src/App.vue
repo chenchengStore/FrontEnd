7d323c86d93746cdff42d8b39e39138d572bb329
chenchengStore <139768726+chenchengStore@users.noreply.github.com>
2023-07-24 15:07:27 +0800

    Add files via upload

---
+++ gobang/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <qipan></qipan>
+</template>
+
+<script setup>
+import qipan from './components/qipan.vue'
+</script>
+
+<style>
+
+</style>
---
+++ gobang/src/components/qizi.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="qizi" style="background-color:black">
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.qizi{
+  position:absolute;
+  height: 20px;
+  width: 20px;
+  border-radius: 100%;
+}
+</style>
